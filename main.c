53adea7fc6a898e12f8f584fcdc872b764af9d0d
Antoni Duda <antoniduda03@protonmail.com>
2024-05-28 17:30:11 +0200

    Added deapply function to remove tetrimone from map after displaying

--- main.c
+++ main.c
@@ -20,29 +20,31 @@ extern void shift_left(uint8_t *tetrimone);
 extern void shift_right(uint8_t *tetrimone);
 extern void sift_down(uint8_t *tetrimone);
 extern void display(uint8_t *tetrimone);
+extern void deapply(uint8_t *tetrimone);
 
 #define BUTTON_GPIO 2
 
 int main() {
-  gpio_init(BUTTON_GPIO);
-  gpio_set_dir(BUTTON_GPIO, GPIO_IN);
-    // We are using the button to pull down to 0v when pressed, so ensure that when
-    // unpressed, it uses internal pull ups. Otherwise when unpressed, the input will
-    // be floating.
-  gpio_pull_up(BUTTON_GPIO);
-  while(gpio_get(BUTTON_GPIO)) {};
-  // gpio_init(25);
+  // gpio_init(BUTTON_GPIO);
+  // gpio_set_dir(BUTTON_GPIO, GPIO_IN);
+  //   // We are using the button to pull down to 0v when pressed, so ensure that when
+  //   // unpressed, it uses internal pull ups. Otherwise when unpressed, the input will
+  //   // be floating.
+  // gpio_pull_up(BUTTON_GPIO);
+  // while(gpio_get(BUTTON_GPIO)) {};
+  // // gpio_init(25);
   // gpio_set_dir(25, GPIO_OUT);
   // gpio_put(25, 1);
   //
-  stdio_uart_init();
+  initUart();
   uint8_t *t = get_tetrimone();
+  display(t);
   t = rotate_left(t);
   shift_left(t);
   sift_down(t);
   for(;;) {
-    serial_write('a');
-    serial_flush();
+    display(t);
+    serial_write('\n');
     // puts("s");
   }
 }
