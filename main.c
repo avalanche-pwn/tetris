6d2c1016ddedf0c986401d51e2b5ddb9c8f8ace1
Antoni Duda <antoniduda03@protonmail.com>
2024-05-28 16:54:00 +0200

    Minor fixes in io connection

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-#include "display.h"
+
 #include "pico/stdio.h"
 #include "pico/stdlib.h"
 #include <stdio.h>
@@ -21,19 +21,27 @@ extern void shift_right(uint8_t *tetrimone);
 extern void sift_down(uint8_t *tetrimone);
 extern void display(uint8_t *tetrimone);
 
-
+#define BUTTON_GPIO 2
 
 int main() {
-  initUart();
+  gpio_init(BUTTON_GPIO);
+  gpio_set_dir(BUTTON_GPIO, GPIO_IN);
+    // We are using the button to pull down to 0v when pressed, so ensure that when
+    // unpressed, it uses internal pull ups. Otherwise when unpressed, the input will
+    // be floating.
+  gpio_pull_up(BUTTON_GPIO);
+  while(gpio_get(BUTTON_GPIO)) {};
+  // gpio_init(25);
+  // gpio_set_dir(25, GPIO_OUT);
+  // gpio_put(25, 1);
   //
-  //stdio_uart_init();
+  stdio_uart_init();
   uint8_t *t = get_tetrimone();
   t = rotate_left(t);
   shift_left(t);
   sift_down(t);
   for(;;) {
-    display(t);
-    serial_write('\n');
+    serial_write('a');
     serial_flush();
     // puts("s");
   }
