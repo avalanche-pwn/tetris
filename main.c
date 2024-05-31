029f8c54fc93a7ee1f9f4ffee54159d5787f330e
Antoni Duda <antoniduda03@protonmail.com>
2024-05-31 16:03:22 +0200

    Fixing me being an idiot

--- main.c
+++ main.c
@@ -4,7 +4,6 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-
 #include "pico/stdio.h"
 #include "pico/stdlib.h"
 #include <stdio.h>
@@ -15,36 +14,38 @@ extern void serial_flush();
 extern uint8_t get_random();
 extern void enable_rosc();
 extern uint8_t *get_tetrimone();
+extern uint8_t *rotate_right(uint8_t *tetrimone);
 extern uint8_t *rotate_left(uint8_t *tetrimone);
 extern void shift_left(uint8_t *tetrimone);
 extern void shift_right(uint8_t *tetrimone);
 extern void sift_down(uint8_t *tetrimone);
 extern void display(uint8_t *tetrimone);
 extern void deapply(uint8_t *tetrimone);
+extern void check_colliding(uint8_t *tetrimone);
 
 #define BUTTON_GPIO 2
 
 int main() {
-  // gpio_init(BUTTON_GPIO);
-  // gpio_set_dir(BUTTON_GPIO, GPIO_IN);
-  //   // We are using the button to pull down to 0v when pressed, so ensure that when
-  //   // unpressed, it uses internal pull ups. Otherwise when unpressed, the input will
-  //   // be floating.
-  // gpio_pull_up(BUTTON_GPIO);
-  // while(gpio_get(BUTTON_GPIO)) {};
+  gpio_init(BUTTON_GPIO);
+  gpio_set_dir(BUTTON_GPIO, GPIO_IN);
+  // We are using the button to pull down to 0v when pressed, so ensure that
+  // when unpressed, it uses internal pull ups. Otherwise when unpressed, the
+  // input will be floating.
+  gpio_pull_up(BUTTON_GPIO);
+  while (gpio_get(BUTTON_GPIO)) {
+  };
   // // gpio_init(25);
   // gpio_set_dir(25, GPIO_OUT);
   // gpio_put(25, 1);
   //
   initUart();
   uint8_t *t = get_tetrimone();
+  // // display(t);
   display(t);
-  t = rotate_left(t);
-  shift_left(t);
-  sift_down(t);
-  for(;;) {
-    display(t);
-    serial_write('\n');
-    // puts("s");
-  }
+  t = rotate_right(t);
+  // t = rotate_left(t);
+  // shift_right(t);
+  // shift_right(t);
+  // check_colliding(t);
+  display(t);
 }
